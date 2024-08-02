ccc78bd7c7be5d04be263d4088cdb0f275906d80
Raphael Nembhard <nembhardraphael199@gmail.com>
2024-08-02 08:25:16 -0400

    Added Two player function; but panics in player one enters not a number

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
-use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
+
+// We want 2 players in this guessing game.
+//  Player 1 to set the number
+//  Player 2 to guess the number
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    loop { 
-        println!("Please input your guess.");
-
-    let mut guess = String::new();
+    let mut secret_number = Default::default();
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read linne");
+        .read_line(&mut secret_number)
+        .expect("Not a number");
 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
+        let secret_number: u32 = match secret_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => panic!("Problem opening the file:"),
+        };
 
-    println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            println!("You win!");
-            break;
-}
-}
-}
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read linne");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
